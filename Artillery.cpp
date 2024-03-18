697b18f345684bac5b62ebfe56ca0a1f9107d456
Michael <michael@chapmanmedia.org>
2024-03-18 11:11:25 -0700

    Finally did math :)

--- Artillery.cpp
+++ Artillery.cpp
@@ -7,6 +7,7 @@
  * https://github.com/GlacierPark19/TheGame
  * This program (including a modified variant) is also avalible on GitHub
  * This function utilizes libraries from the GNU C++ Libraries
+ * This program code can be viewed at: https://github.com/GlacierPark19/MasterGunner
  */
 
 #include <iostream>
@@ -17,10 +18,12 @@ using namespace std;
 
 int main()
 {
+    int high, low;
+    int graceValue = 0;
     string userInput;
     double velocity = 0.0;
     double angle = 0.0;
-    int difficultyLevel = 10;
+    int difficultyLevel = 99;
     int fireDistance = 0;
     int minDistance = 100, maxDistance = 200, roundNumber = 0, i;
     string difficulty = "";
@@ -41,96 +44,128 @@ int main()
 
     cout << banner << endl
          << endl;
-    while (win < 1)
-    {
 
-        cout << tankPosition << " DEBUG" << endl;
-        cout << "Enter your name commander! " << endl;
-        getline(cin, playerName);
-        cout << "Welcome to the unit " << playerName << "!\n The enemy aproaches and we need your expertise!\n";
-        cout << "Choose your difficulty!\n";
-        cout << endl;
-        cout << endl;
-        cout << "Choose Difficulty: Easy, Medium, or Hard" << endl;
-        getline(cin, difficulty);
-        // This function fixes issues regarding capitilization.
-        transform(difficulty.begin(), difficulty.end(), difficulty.begin(), ::towlower);
-        // Then, to make my life "Easy" I assign each difficulty a nemerical value. Why do I do this? BC I hate comparing strings. Too many possibilities
-        if (difficulty == "easy")
+    cout << tankPosition << " DEBUG" << endl;
+    cout << "Enter your name commander! " << endl;
+    getline(cin, playerName);
+    cout << "Welcome to the unit " << playerName << "!\n The enemy aproaches and we need your expertise!\n";
+    cout << "Choose your difficulty!\n";
+    cout << endl;
+    cout << endl;
+    cout << "Choose Difficulty: Easy, Medium, or Hard" << endl;
+    getline(cin, difficulty);
+    // This function fixes issues regarding capitilization.
+    transform(difficulty.begin(), difficulty.end(), difficulty.begin(), ::towlower);
+    // Then, to make my life "Easy" I assign each difficulty a nemerical value. Why do I do this? BC I hate comparing strings. Too many possibilities
+    if (difficulty == "easy")
+    {
+        difficultyLevel = 0;
+        graceValue = 20;
+    }
+    else
+    {
+        if (difficulty == "medium")
         {
-            difficultyLevel = 0;
+            difficultyLevel = 1;
+            graceValue = 10;
         }
         else
         {
-            if (difficulty == "medium")
+            if (difficulty == "hard")
             {
-                difficultyLevel = 1;
-            }
-            else
-            {
-                if (difficulty == "hard")
-                {
-                    difficultyLevel = 2;
-                }
-            }
-        }
-        for (i = 0; i <= 3; i++)
-        {
-            cout << "Good Choice commander! " << difficulty << " Is not for everyone!\n Your tank is ready! Head for the field of battle to Victory!\n";
-            // cout << "\033[2J\033[1;1H"; // clears the screen utilizing some form of console black magic bulls**t. Pardon my french
-            cout << "Enter your turret angle!" << endl;
-            getline(cin, userInput);
-            angle = stod(userInput);
-            cout << "Your Firing angle is " << angle << " degrees!" << endl;
-            cout << "Enter your firing power! (Velocity)" << endl;
-            getline(cin, userInput);
-            velocity = stod(userInput);
-            cout << "Commander we have a shooting solution! Our angle is: " << angle << " Degrees and our velcotiy is " << velocity << "! Firing!" << endl;
-            fireDistance = (velocity * cos(angle * (M_PI / 180)));
-            cout << "Commander! Our payload traveled " << fireDistance << " miles!" << endl;
-            switch (difficultyLevel)
-            {
-            case 0:
-                if ((fireDistance == tankPosition) || (fireDistance == 0))
-                {
-                    cout << "Congrats Commander! A succsessful hit! The Enenmy has been defeated!" << endl;
-                    cout << "The enemy was located at a distance of: " << fireDistance << " Miles! You have won!" << endl;
-                    cout << "----YOU WIN!------" << endl
-                         << "-------GAME OVER--------";
-                }
-                else
-                {
-                    cout << "Our shot missed! ";
-                    if (fireDistance < tankPosition)
-                    {
-                        cout << "Our shot was too Short!" << endl;
-                    }
-                    if (fireDistance > tankPosition)
-                    {
-                        cout << "Our shot was too Long!" << endl;
-                    }
-                }
-                break;
-            case 1:
-                if ((fireDistance == tankPosition) || (fireDistance == 0))
-                {
-                    cout << "Congrats Commander! A succsessful hit! The Enenmy has been defeated!" << endl;
-                    cout << "The enemy was located at a distance of: " << fireDistance << " Miles! You have won!" << endl;
-                    cout << "----YOU WIN!------" << endl
-                         << "-------GAME OVER--------";
-                }
-                break;
-            case 3:
-                if ((fireDistance == tankPosition) || (fireDistance == 0))
-                {
-                    cout << "Congrats Commander! A succsessful hit! The Enenmy has been defeated!" << endl;
-                    cout << "The enemy was located at a distance of: " << fireDistance << " Miles! You have won!" << endl;
-                    cout << "----YOU WIN!------" << endl
-                         << "-------GAME OVER--------";
-                }
-                break;
+                difficultyLevel = 2;
+                graceValue = 5;
             }
         }
     }
-    return 0;
+    for (i = 0; i <= 2; i++)
+    {
+        cout << "Good Choice commander! " << difficulty << " Is not for everyone!\n Your tank is ready! Head for the field of battle to Victory!\n";
+        // cout << "\033[2J\033[1;1H"; // clears the screen utilizing some form of console black magic bulls**t. Pardon my french
+        cout << "Enter your turret angle!" << endl;
+        getline(cin, userInput);
+        angle = stod(userInput);
+        cout << "Your Firing angle is " << angle << " degrees!" << endl;
+        cout << "Enter your firing power! (Velocity)" << endl;
+        getline(cin, userInput);
+        velocity = stod(userInput);
+        cout << "Commander we have a shooting solution! Our angle is: " << angle << " Degrees and our veloctiy is " << velocity << "! Firing!" << endl;
+        cout << "DEBUG: ENTER TEST POSITION: ";
+        getline(cin, userInput);
+        fireDistance = stoi(userInput);
+        // fireDistance = (velocity * cos(angle * (M_PI / 180)));
+        cout << "Commander! Our payload traveled " << fireDistance << " miles!" << endl;
+        int high = (tankPosition + graceValue);
+        int low = (fireDistance - graceValue);
+        switch (difficultyLevel)
+        {
+        case 0:
+            if ((fireDistance == tankPosition) || (low <= tankPosition <= high))
+            {
+                cout << "Congrats Commander! A succsessful hit! The Enenmy has been defeated!" << endl;
+                cout << "The enemy was located at a distance of: " << tankPosition << " Miles! You have won!" << endl;
+                cout << "----YOU WIN!------" << endl
+                     << "-------GAME OVER--------";
+            }
+            else
+            {
+                cout << "Our shot missed! ";
+                if (fireDistance < tankPosition)
+                {
+                    cout << "Our shot was too Short!" << endl;
+                }
+                if (fireDistance > tankPosition)
+                {
+                    cout << "Our shot was too Long!" << endl;
+                }
+            }
+            break;
+        case 1:
+            if ((fireDistance == tankPosition) || (low <= tankPosition <= high))
+            {
+                cout << "Congrats Commander! A succsessful hit! The Enenmy has been defeated!" << endl;
+                cout << "The enemy was located at a distance of: " << tankPosition << " Miles! You have won!" << endl;
+                cout << "----YOU WIN!------" << endl
+                     << "-------GAME OVER--------";
+            }
+            else
+            {
+                cout << "Our shot missed! ";
+                if (fireDistance < tankPosition)
+                {
+                    cout << "Our shot was too Short!" << endl;
+                }
+                if (fireDistance > tankPosition)
+                {
+                    cout << "Our shot was too Long!" << endl;
+                }
+            }
+            break;
+        case 3:
+            if ((fireDistance == tankPosition) || (low <= tankPosition <= high))
+            {
+                cout << "Congrats Commander! A succsessful hit! The Enenmy has been defeated!" << endl;
+                cout << "The enemy was located at a distance of: " << tankPosition << " Miles! You have won!" << endl;
+                cout << "----YOU WIN!------" << endl
+                     << "-------GAME OVER--------";
+            }
+            else
+            {
+                cout << "Our shot missed! ";
+                if (fireDistance < tankPosition)
+                {
+                    cout << "Our shot was too Short!" << endl;
+                }
+                if (fireDistance > tankPosition)
+                {
+                    cout << "Our shot was too Long!" << endl;
+                }
+            }
+            break;
+        }
+        cout << "****************************" << endl
+             << endl
+             << endl
+             << endl;
+    }
 }
